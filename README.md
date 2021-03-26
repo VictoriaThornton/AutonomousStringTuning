82b27230c8f33b489a7378e161fd1d8db906ebf8
Victoria Thornton <joy12vjt@yahoo.com>
2021-03-26 14:56:59 -0400

    Edited note ranges, added keypresses and text animation
    Notes:
    -note ranges are from lab testing
    -keypresses send a noteOff signal on channels 1,2,3,4,5 or 6
    -changed webmidi include to a link instead of the file

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 ### Current Functionality: 
 * Custom Mode: select notes for all 5 strings
+* Keypresses:
+    * keys 1, 2, 3, 4, or 5 actuate individual strings
+    * keys a or 6 actuate all strings at once 
 * Send notes to Arduino Due by pressing select tuning button
 
 ### Notes: 
 * MIDI notes sent are in the range of 0 to 7
 * MIDI channel is the string number
+
--- UI.js
+++ UI.js
@@ -1,137 +1,197 @@
-var dropdownState = 'expanded'; 
-var chosenNotes; 
+var dropdownState = "expanded";
+var chosenNotes;
 
-function init(){
-    toggleDropdownDisplay(); 
-    selectMode('custom');
-    chosenNotes = [0,2,4,7,6]; //default note values;
-    clearCursors();
-    document.getElementById("string1note1").style.opacity = 100; 
-    document.getElementById("string2note3").style.opacity = 100; 
-    document.getElementById("string3note5").style.opacity = 100; 
-    document.getElementById("string4note8").style.opacity = 100;
-    document.getElementById("string5note7").style.opacity = 100;    
+//defined chords
+var chordA = ["A", "E", "A", "C#", "E"];
+var chordAm = ["A", "E", "A", "C", "E"];
+var chordB = ["B", "F#", "B", "D#", "F#"];
+var chordBm = ["B", "F#", "B", "D", "F#"];
+var chordC = ["C", "E", "G", "C", "E"];
+var chordCm = ["A#", "E", "G", "C", "E"];
+var chordD = ["x", "D", "A", "D", "F#"];
+var chordDm = ["x", "D", "A", "D", "F"];
+var chordE = ["B", "E", "G#", "B", "E"];
+var chordEm = ["B", "E", "G", "B", "E"];
+var chordF = ["C", "G", "A", "C", "F"];
+var chordFm = ["C", "G", "G#", "C", "F"];
+var chordG = ["B", "D", "G", "B", "G"];
+var chordGm = ["C", "F", "G#", "B", "F"];
+
+function init() {
+  toggleDropdownDisplay();
+  selectMode("custom");
+  chosenNotes = [0, 2, 4, 7, 6]; //default note values;
+  clearCursors();
+  document.getElementById("string1note1").style.opacity = 100;
+  document.getElementById("string2note3").style.opacity = 100;
+  document.getElementById("string3note5").style.opacity = 100;
+  document.getElementById("string4note8").style.opacity = 100;
+  document.getElementById("string5note7").style.opacity = 100;
+
+    //keypresses to actuate solenoids
+    window.onkeypress = function(event){
+        var key = event.key; 
+        switch(key){
+            case '1': 
+            actuateSolenoid(1);
+            console.log("actuate string 1 solenoid"); 
+            break; 
+            
+            case '2': 
+            actuateSolenoid(2);
+            console.log("actuate string 2 solenoid"); 
+            break; 
+
+            case '3': 
+            actuateSolenoid(3);
+            console.log("actuate string 3 solenoid"); 
+            break; 
+
+            case '4': 
+            actuateSolenoid(4);
+            console.log("actuate string 4 solenoid"); 
+            break; 
+
+            case '5':
+                actuateSolenoid(5);
+                console.log("actuate string 5 solenoid"); 
+            break;  
+
+            case '6': case 'a': case 'A': 
+                actuateSolenoid(6);
+                console.log("actuate all solenoids");  
+            break; 
+        }
+    }
 }
 
-function selectNote(note, string){ 
-    //call to add the notes chosen by the user to an array once it's selected. One note for each string. Once the user hits the button
-    chosenNotes[string-1] = note; //note starts from zero here to match 0 to 12 note scale
-    clearStringCursors(string);     
-    var displayNote = note + 1; //note + 1 corresponds to the displayed notes on the UI
+function selectNote(note, string) {
+    //add the notes chosen by the user to an array
+    chosenNotes[string - 1] = note; //note starts from zero here to match 0 to 12 note scale; string numbers start from 1
+    clearStringCursors(string);
+    var displayNote = note + 1; //note + 1 corresponds how the note is displayed on the UI. N1 instead of N0
     console.log("Selected note ", displayNote, "String:  ", string);
-    document.getElementById("string"+string+"note"+displayNote).style.opacity = 100; 
-
-    // if(string == 3){
-    //     var noteNames = document.getElementsByClassName("noteNameString3"); 
-    //     for(var i = 0; i < noteNames.length; i++){
-    //         cursors[note].style = "text-size-adjust: 80%;"
-    //     }
-    // }
+    document.getElementById("string" + string + "note" + displayNote).style.opacity = 100; //make the cursor appear selected
+    document.getElementById("string" + string + "noteLabel" + displayNote).style.fontSize = "24px"; //enlarge selected note text
 }
 
-function chords(){} //have preset chord configurations to choose from
+//preset chord configurations
+function chords() {}
 
-function clearStringCursors(string){
-    //add in clear options for rest of strings
-    var cursors = document.getElementsByClassName("cursor-"+string); 
-    for(var i=0; i < cursors.length; i++){
-        cursors[i].style.opacity = 0;
-    }
+function clearStringCursors(string) {
+  //reset cursor selections for current string
+  var cursors = document.getElementsByClassName("cursor-" + string);
+  for (var i = 0; i < cursors.length; i++) {
+    cursors[i].style.opacity = 0;
+  }
+
+  //reset text sizes for current string
+  var noteLabels = document.getElementsByClassName("label-" + string);
+  for (var i = 0; i < noteLabels.length; i++) {
+    noteLabels[i].style.fontSize = "18px";
+  }
 }
 
-function clearCursors(){
-    //add in clear options for rest of strings
-    var cursors = document.getElementsByClassName("cursor"); 
-    for(var i=0; i < cursors.length; i++){
-        cursors[i].style.opacity = 0;
-    }
+function clearCursors() {
+  //add in clear options for rest of strings
+  var cursors = document.getElementsByClassName("cursor");
+  for (var i = 0; i < cursors.length; i++) {
+    cursors[i].style.opacity = 0;
+  }
 }
 
-function selectTuningButtonPressed(){
-    document.getElementById("play-button").style.display = "none";
-    document.getElementById("play-button-pressed").style.display = "block"; 
-    document.getElementById("play-button-pressed").webkitTransform = "translateY(0.5px)"; 
-    document.getElementById("play-button-pressed").webkitTransform = "translateY(-0.5px)"; 
+function selectTuningButtonPressed() {
+  document.getElementById("play-button").style.display = "none";
+  document.getElementById("play-button-pressed").style.display = "block";
+  //button animation
+  document.getElementById("play-button-pressed").className += " button-pressed";
+
+  //send MIDI tuning to Arduino
+  for (var i = 0; i < chosenNotes.length; i++) {
+    var stringNumber = i + 1;
+    var note = chosenNotes[i];
+    var displayNote = note + 1;
+    console.log("Selected Note: ",displayNote,"for String number ",stringNumber);
     
-    //send MIDI tuning to Arduino
-    for(var i = 0; i < chosenNotes.length; i++) {
-        var stringNumber = i+1;
-        var note = chosenNotes[i]; 
-        var displayNote = note + 1; 
-        console.log("Selected Note: ", displayNote, "for String number ", stringNumber); 
-        if (midiOutput) {
-            midiOutput.playNote(note, stringNumber, { duration: 100, velocity: 0.5 });
-        }
-        else {
-            console.log("Looks like there is no MIDI output device. Check if your Arduino is connected.");
-        }
+    if (midiOutput) {
+      midiOutput.playNote(note, stringNumber, { duration: 100, velocity: 0.5 });
+    } else {
+      console.log(
+        "Looks like there is no MIDI output device. Check if your Arduino is connected."
+      );
     }
+  }
 }
 
-function finishedTuning(){
-    document.getElementById("play-button").style.display = "block";
-    document.getElementById("play-button-pressed").style.display = "none";  
+//when the system has finished tuning, reset button to allow press again
+function finishedTuning() {
+  document.getElementById("play-button").style.display = "block";
+  document.getElementById("play-button-pressed").style.display = "none";
 }
 
-function selectMode(type){
-    if(type == 'chord'){
-        document.getElementById("chordRadioButton").style.opacity = 100;
-        document.getElementById("customRadioButton").style.opacity = 0;
-    }
-    if(type == 'custom'){
-        document.getElementById("chordRadioButton").style.opacity = 0;
-        document.getElementById("customRadioButton").style.opacity = 100;
-    }
+function selectMode(type) {
+  if (type == "chord") {
+    document.getElementById("chordRadioButton").style.opacity = 100;
+    document.getElementById("customRadioButton").style.opacity = 0;
+  }
+  if (type == "custom") {
+    document.getElementById("chordRadioButton").style.opacity = 0;
+    document.getElementById("customRadioButton").style.opacity = 100;
+  }
 }
 
-function toggleDropdownDisplay(){
-    var elements = document.getElementsByClassName('expand');
+function toggleDropdownDisplay() {
+  var elements = document.getElementsByClassName("expand");
 
-    if(dropdownState == 'collapsed'){
-        var elements = document.getElementsByClassName('expand');
-        for (var i=0;i<elements.length;i+=1){
-        elements[i].style.opacity = 100;
-        elements[i].style.display = "block"; 
-        }
-        dropdownState = 'expanded'; 
-        return; 
+  if (dropdownState == "collapsed") {
+    var elements = document.getElementsByClassName("expand");
+    for (var i = 0; i < elements.length; i += 1) {
+      elements[i].style.opacity = 100;
+      elements[i].style.display = "block";
     }
+    dropdownState = "expanded";
+    return;
+  }
 
-    if(dropdownState == 'expanded'){
-        
-        for (var i=0;i < elements.length;i+=1){
-          //  elements[i].style.display = 'none';
-          elements[i].style.opacity = 0;
-          elements[i].style.display = "none";         
-        }
-        dropdownState = 'collapsed';
-        return; 
+  if (dropdownState == "expanded") {
+    for (var i = 0; i < elements.length; i += 1) {
+      elements[i].style.opacity = 0;
+      elements[i].style.display = "none";
     }
+    dropdownState = "collapsed";
+    return;
+  }
 }
 
-function dropdownSelection(selection){
-    document.getElementById("first-drop-down-box-text").innerHTML = selection; 
-    toggleDropdownDisplay(); 
+//allow selection of chords
+function dropdownSelection(selection) {
+  document.getElementById("first-drop-down-box-text").innerHTML = selection;
+  toggleDropdownDisplay();
 }
 
-function actuateSolenoid(){
-    
+//actuate the solenoid using keypresses for the different strings.
+//1, 2, 3, 4, 5 to correspond to the different strings. Sends noteOff signals where the channel numbers correspond to the string number
+//channel 6 corresponds to striking all of the strings at once
+function actuateSolenoid(stringNumber) {
+    if (midiOutput) {
+        midiOutput.stopNote(10, stringNumber, { duration: 100, velocity: 0.5 });
+    } else {
+        console.log(
+          "Looks like there is no MIDI output device. Check if your Arduino is connected."
+        );
+      }
 }
 
 WebMidi.enable(function (err) {
-    if (err) {
-        console.log("WebMidi could not be enabled.", err);
-    }
+  if (err) {
+    console.log("WebMidi could not be enabled.", err);
+  }
 
-    // Print available MIDI outputs
-    for (let i = 0; i < WebMidi.outputs.length; i++) {
-        console.log(WebMidi.outputs[i].name);
-    }
+  // Print available MIDI outputs
+  for (let i = 0; i < WebMidi.outputs.length; i++) {
+    console.log(WebMidi.outputs[i].name);
+  }
 
-    // From the list on the console, pick an output name:
-    // midiOutput = WebMidi.getOutputByName("IAC Driver IAC Bus 1");
-    midiOutput = WebMidi.getOutputByName("Arduino Due");
-
-
+  // From the list on the console, pick an output name:
+  midiOutput = WebMidi.getOutputByName("Arduino Due");
 });
-
